547ba20ab1d059d97bbbcc19a42716a17f102f71
Ian Koh <ian_koh93@hotmail.com>
2022-02-16 17:24:51 -0800

    finished a working quiz generator

--- data.py
+++ data.py
@@ -1,14 +1,181 @@
-question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+# question_data = [
+#     {"category": "Entertainment: Video Games",
+#      "type": "boolean",
+#      "difficulty": "easy",
+#      "question": "Niko Bellic is the protagonist of Grand Theft Auto IV.",
+#      "correct_answer": "True", "incorrect_answers": ["False"]},
+#     {"category": "General Knowledge",
+#      "type": "boolean",
+#      "difficulty": "easy",
+#      "question": "When you cry in space, your tears stick to your face.",
+#      "correct_answer": "True",
+#      "incorrect_answers": ["False"]},
+#     {"category": "Entertainment: Film",
+#      "type": "boolean",
+#      "difficulty": "easy",
+#      "question": "In the movie The Revenant, DiCaprio's character hunts down the killer of his son.",
+#      "correct_answer": "True",
+#      "incorrect_answers": ["False"]},
+#     {"category": "Science: Computers",
+#      "type": "boolean",
+#      "difficulty": "medium",
+#      "question": "The first dual-core CPU was the Intel Pentium D.",
+#      "correct_answer": "False",
+#      "incorrect_answers": ["True"]},
+#     {"category": "Sports",
+#      "type": "boolean",
+#      "difficulty": "medium",
+#      "question": "Formula E is an auto racing series that uses hybrid electric race cars.",
+#      "correct_answer": "False",
+#      "incorrect_answers": ["True"]},
+#     {"category": "History",
+#      "type": "boolean",
+#      "difficulty": "medium",
+#      "question": "The Hundred Years War was fought for more than a hundred years.",
+#      "correct_answer": "True",
+#      "incorrect_answers": ["False"]},
+#     {"category": "Entertainment: Video Games",
+#      "type": "boolean",
+#      "difficulty": "easy",
+#      "question": "In Pokemon, Arbok evolves into Seviper.",
+#      "correct_answer": "False",
+#      "incorrect_answers": ["True"]},
+#     {"category": "Entertainment: Cartoon & Animations",
+#      "type": "boolean", "difficulty": "easy",
+#      "question": "Waylon Smithers from 'The Simpsons' was originally black when he first appeared in the series.",
+#      "correct_answer": "True",
+#      "incorrect_answers": ["False"]},
+#     {"category": "Science & Nature",
+#      "type": "boolean",
+#      "difficulty": "easy",
+#      "question": "Water always boils at 100C, 212F, 373.15K, no matter where you are.",
+#      "correct_answer": "False",
+#      "incorrect_answers": ["True"]},
+#     {"category": "Entertainment: Video Games",
+#      "type": "boolean",
+#      "difficulty": "medium",
+#      "question": "The game Garry's Mod originally took assets and codes from the popular Half Life 2 mod JBmod.",
+#      "correct_answer": "True",
+#      "incorrect_answers": ["False"]}]
+
+question_data = {
+  "response_code": 0,
+  "results": [
+    {
+      "category": "Entertainment: Music",
+      "type": "boolean",
+      "difficulty": "medium",
+      "question": "Soulja Boy&#039;s &#039;Crank That&#039; won a Grammy for Best Rap Song in 2007.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "category": "Geography",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "Vatican City is a country.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "category": "Art",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "Leonardo da Vinci&#039;s Mona Lisa does not have eyebrows.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "category": "Entertainment: Video Games",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "In &quot;Mario Kart 64&quot;, Waluigi is a playable character.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "category": "General Knowledge",
+      "type": "boolean",
+      "difficulty": "medium",
+      "question": "The word &quot;news&quot; originates from the first letters of the 4 main directions on a compass (North, East, West, South).",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "category": "Vehicles",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "The full English name of the car manufacturer BMW is Bavarian Motor Works",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "category": "Geography",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "St. Louis is the capital of the US State Missouri.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "category": "Entertainment: Television",
+      "type": "boolean",
+      "difficulty": "medium",
+      "question": "In &quot;Star Trek: The Next Generation&quot;, Data is the only android in existence.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "category": "Mythology",
+      "type": "boolean",
+      "difficulty": "hard",
+      "question": "Janus was the Roman god of doorways and passageways.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "category": "Science & Nature",
+      "type": "boolean",
+      "difficulty": "medium",
+      "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; refers to a resting heart-rate near or over 100 BPM.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    }
+  ]
+}
+
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# When the problem with the last of the
+# Quiz Gameshow
+
+import quiz_brain, data, question_model
+
+question_bank = []
+
+for i in range(0, len(data.question_data["results"])):
+    question_text = data.question_data["results"][i]["question"]
+    answer_text = data.question_data["results"][i]["correct_answer"]
+    question_obj = question_model.Question(question_text, answer_text)
+    question_bank.append(question_obj)
+
+# for i in range(0, len(question_bank)):
+#     print(question_bank[i].text)
+#     print(question_bank[i].answer)
+
+quiz = quiz_brain.QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
